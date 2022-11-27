4cf9a4a0a7bfc76798bd09d653bfd3e0358e7087
Artem Ismagilov <a.ismagilow@gmail.com>
2022-11-27 09:45:59 +0300

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -34,7 +34,6 @@ unzip alignments.zip >> /dev/null
 # we will use waveglow code, data and audio preprocessing from this repo
 git clone https://github.com/xcmyz/FastSpeech.git
 mv FastSpeech/text .
-mv FastSpeech/audio .
 mv FastSpeech/waveglow/* waveglow/
 mv FastSpeech/utils.py .
 mv FastSpeech/glow.py .
